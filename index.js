d1a182491bf12b9b2438154272858eebcc7abfa4
avengingtooth <esellin@gmail.com>
2023-01-18 15:00:57 +0100

    Iteration1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let hacker1 = 'Emma'
+console.log("The driver's name is", hacker1)
+let hacker2 = 'Soizic'
+console.log("The navigator's name is", hacker2)
 
 // Iteration 2: Conditionals
 
