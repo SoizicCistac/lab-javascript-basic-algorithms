7223224c83c48d9d2018fcfc57c0634c53a76f02
avengingtooth <esellin@gmail.com>
2023-01-18 15:18:14 +0100

    3rdIterations

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let hacker2 = 'Soizic'
 console.log("The navigator's name is", hacker2)
 
 // Iteration 2: Conditionals
+
 if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
 } else if (hacker2.length > hacker1.length) {
@@ -14,3 +15,26 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+let txt = ''
+
+for (let i = 0; i < hacker1.length; i++){
+    txt += hacker1[i].toUpperCase() + ' '
+}
+
+console.log(txt)
+txt = ''
+
+for (let i = hacker2.length - 1; i >= 0; i--){
+    txt += hacker2[i]
+}
+console.log(txt)
+
+if (hacker1[0] < hacker2[0]){
+    console.log("The driver's name goes first.")
+}
+else if (hacker1[0] > hacker2[0]){
+    console.log("Yo, the navigator goes first definitely.")
+}
+else{
+    console.log("What?! You both have the same name?")
+}
